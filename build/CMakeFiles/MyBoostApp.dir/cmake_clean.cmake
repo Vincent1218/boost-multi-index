6d08fd1c4d4c45d44679b4b11820a3ce1b4556a4
Chai Wen Xuan <vicentchai@hotmail.com>
2024-09-07 10:06:37 +0800

    Testing Boost multi index container

---
+++ build/CMakeFiles/MyBoostApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyBoostApp.dir/main.cpp.o"
+  "CMakeFiles/MyBoostApp.dir/main.cpp.o.d"
+  "MyBoostApp"
+  "MyBoostApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyBoostApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
